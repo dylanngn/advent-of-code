b44a4e02c44656b2ed0fdb8f41327623c534adab
m1ng24 <117571344+m1ng24@users.noreply.github.com>
2023-12-05 17:38:19 +0700

    WIP

--- src/day5/index.ts
+++ src/day5/index.ts
@@ -1,10 +1,17 @@
-import * as path from "path";
-import { parseInput } from "../util";
+import * as path from 'path';
+import { parseInput } from '../util';
 
-const input = parseInput(path.join(__dirname, "input.txt"));
+const input = parseInput(path.join(__dirname, 'input.txt'), {
+  split: {
+    delimiter: '\n\n',
+    mapper: false,
+  },
+});
 
 function main() {
-  console.log(input);
+  // console.log(input);
+  const seeds = input[0].split(':')[1].trim().split(' ');
+  const maps = input.slice(1).map((e) => e.split(':')[1].trim().split('\n'));
   return 0;
 }
 
